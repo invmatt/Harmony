e09ab6d18b09f5e6d5fe88201be1890f88df7a2c
Matt <matt.otley@cubik.co.uk>
2013-11-14 12:35:50 +0000

    Fixed errors / Rewrote single.php

--- single.php
+++ single.php
@@ -1,33 +1,65 @@
 <?php get_header(); ?>
-
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-        <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-
-                <div class="entry-content">
-
-                        <?php the_content(); ?>
-
-                        <?php wp_link_pages(array('before' => __('Pages: '), 'next_or_number' => 'number')); ?>
-
-                        <?php the_tags( __('Tags: '), ', ', ''); ?>
-
-                        <?php posted_on(); ?>
-
-                </div>
-
-                <?php edit_post_link(__('Edit this entry'),'','.'); ?>
-
-        </article>
-
-        <?php comments_template(); ?>
-
-<?php endwhile; endif; ?>
-
-<?php post_navigation(); ?>
-
+        
+        <!-- section -->
+        <section role="main">
+        
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+        
+                <!-- article -->
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                
+                        <!-- post thumbnail -->
+                        <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                        <?php the_post_thumbnail(); // Fullsize image for the single post ?>
+                                </a>
+                        <?php endif; ?>
+                        <!-- /post thumbnail -->
+                        
+                        <!-- post title -->
+                        <h1>
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                        </h1>
+                        <!-- /post title -->
+                        
+                        <!-- post details -->
+                        <span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+                        <span class="author"><?php _e( 'Published by', 'harmony' ); ?> <?php the_author_posts_link(); ?></span>
+                        <span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'harmony' ), __( '1 Comment', 'harmony' ), __( '% Comments', 'harmony' )); ?></span>
+                        <!-- /post details -->
+                        
+                        <?php the_content(); // Dynamic Content ?>
+                        
+                        <?php the_tags( __( 'Tags: ', 'harmony' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+                        
+                        <p><?php _e( 'Categorised in: ', 'harmony' ); the_category(', '); // Separated by commas ?></p>
+                        
+                        <p><?php _e( 'This post was written by ', 'harmony' ); the_author(); ?></p>
+                        
+                        <?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+                        
+                        <?php comments_template(); ?>
+                        
+                </article>
+                <!-- /article -->
+                
+        <?php endwhile; ?>
+        
+        <?php else: ?>
+        
+                <!-- article -->
+                <article>
+                        
+                        <h1><?php _e( 'Sorry, nothing to display.', 'harmony' ); ?></h1>
+                        
+                </article>
+                <!-- /article -->
+        
+        <?php endif; ?>
+        
+        </section>
+        <!-- /section -->
+        
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
