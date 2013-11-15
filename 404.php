78bdf9de9e91e11ef08342369054373ea5d73b38
Matt <matt.otley@cubik.co.uk>
2013-11-15 15:45:25 +0000

    minor tweaks
    Added Analytics code, 404 class and logo class

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-	<h2><?php _e('Error 404 - Page Not Found','harmony'); ?></h2>
+	<h2 class="error error-404"><?php _e('Error 404 - Page Not Found','harmony'); ?></h2>
 
 <?php get_sidebar(); ?>
 
--- header.php
+++ header.php
@@ -10,12 +10,13 @@
 		<meta charset="utf-8">
 
 		<title><?php wp_title(''); ?></title>
-
+		
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/lib/assets//favicon.png">
 		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/lib/assets/favicon.ico">
 		<![endif]-->
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
@@ -29,7 +30,7 @@
 
 			<header class="header" role="banner">
 
-				<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+				<p id="logo" class="h1 logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
 
 				<nav role="navigation">
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'depth' => 1, 'container_class' => '', 'container_id' => 'main-nav' ) ); ?>
