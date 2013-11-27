501fb14bab92941995589ee6f33b08fe245dafd5
invmatt <matt@invinta.com>
2013-11-27 13:55:46 +0000

    Updated title

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
 		<meta charset="utf-8">
 
-		<title><?php wp_title(''); ?></title>
+		<title><?php echo get_the_title(); ?>  | <?php bloginfo('name'); ?></title>
 		
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
