bed8fd5bf938917f7755294b877f740c6242b5cf
invmatt <matt@invinta.com>
2013-12-31 12:38:21 +0000

    Removed edit post link

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <header class="header">
-                        <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+                        <h1 class="entry-title"><?php the_title(); ?></h1>
                 </header>
                 <section class="entry-content">
                         <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
