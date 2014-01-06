586aa8c447dd3302eb01702249b457b68bca47ff
invmatt <matt@invinta.com>
2014-01-06 11:21:25 +0000

    Added classes to form and elements

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<div>
-		<label for="s" class="visuallyhidden"><?php _e('Search for:','harmony'); ?></label>
-		<input type="search" id="s" name="s" value="" />
-		<input type="submit" value="<?php _e('Search','harmony'); ?>" id="searchsubmit" />
+		<label for="s" class="visuallyhidden search-form__label"><?php _e('Search for:','harmony'); ?></label>
+		<input type="search" id="s" class="search-form__input" name="s" value="" />
+		<input type="submit" class="search-form__btn" value="<?php _e('Search','harmony'); ?>" id="searchsubmit" />
 	</div>
 </form>
