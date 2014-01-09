05d6d014bc0e8a5b1ea5bf83356546b1c57b0eee
invmatt <matt@invinta.com>
2014-01-09 09:55:56 +0000

    Updated author URL

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<footer class="footer" role="contentinfo">
 
 			<p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-			<p class="author">Harmony <?php echo $theme_version; ?> - The Wordpress Framework for Developers</p>
+			<a class="author" href="https://github.com/invmatt/Harmony">Harmony</a>
 
 		</footer>
 
