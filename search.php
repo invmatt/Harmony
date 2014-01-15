770f64692ac2c4b79622de6e510db89f439e5c5c
invmatt <matt@invinta.com>
2014-01-15 14:06:08 +0000

    Cleaned up indentation

--- search.php
+++ search.php
@@ -1,32 +1,26 @@
 <?php get_header(); ?>
 
-        <?php if (have_posts()) : ?>
+<?php if (have_posts()) : ?>
 
-                <h2><?php _e('Search Results','harmony'); ?></h2>
+  <h2><?php _e('Search Results','harmony'); ?></h2>
+  
+  
+  <?php while (have_posts()) : the_post(); ?>
+  
+          <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+                  <h2><?php the_title(); ?></h2>
+                  <div class="entry">
+                          <?php the_excerpt(); ?>
+                  </div>
+          </article>
+  
+  <?php endwhile; ?>
 
+<?php else : ?>
 
-                <?php while (have_posts()) : the_post(); ?>
+  <h2><?php _e('Nothing Found','harmony'); ?></h2>
 
-                        <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-                                <h2><?php the_title(); ?></h2>
-
-
-                                <div class="entry">
-
-                                        <?php the_excerpt(); ?>
-
-                                </div>
-
-                        </article>
-
-                <?php endwhile; ?>
-
-        <?php else : ?>
-
-                <h2><?php _e('Nothing Found','harmony'); ?></h2>
-
-        <?php endif; ?>
+<?php endif; ?>
 
 <?php get_sidebar(); ?>
 
