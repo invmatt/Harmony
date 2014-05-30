38192454f73e4fd9dfd18eb405b857fccc546905
invmatt <matt@invinta.com>
2014-05-30 11:18:00 +0100

    Added JS switch class snippet
    Removes the need for jQuery and replaces the class asap to help prevent content flash

--- header.php
+++ header.php
@@ -8,19 +8,23 @@
 	<head>
 
 		<meta charset="utf-8">
-
+	
 		<title><?php echo get_the_title(); ?>  | <?php bloginfo('name'); ?></title>
 		
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
+	
 		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/lib/assets/favicon.png">
 		<!--[if IE]>
 			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/lib/assets/favicon.ico">
 		<![endif]-->
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
-	    <?php wp_head(); ?>
+		
+		<script>
+			document.documentElement.className = document.documentElement.className.replace(/(\s|^)no-js(\s|$)/, '$1js$2');
+		</script>
+	
+	    	<?php wp_head(); ?>
 
 	</head>
 
